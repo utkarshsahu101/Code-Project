3f8dbcdcf242ebba50c0c2d9f745d2c17d796ca6
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-06-13 18:37:05 +0530

    readme is added and all corrections done

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Description
+
+This is website build using HTML and CSS
+
+> Flexbox is used
+
+> Grid is used in Contact section
+
+> Form service is unavailabe right now
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  $("menu-btn").click(changeNav);
+  $("#links").hide();
+  $("#menu-btn").click(changeNav);
   function changeNav() {
     $("#links").toggle(2000);
     $("#menu-btn").toggleClass("turn");
